ce128381a89c7ed11fe48ce5ebcc4782c0712938
PRAVEEN-PUVVALA <praveen.p@hiscopetech.com>
2025-03-07 16:12:37 +0530

    pakages

---
+++ src/EVAAP.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import './EVAAP.css';
+
+class EVAAP extends Component {
+    render() {
+        return (
+        <div>
+            <h1>EVAAP</h1>
+        </div>
+        );
+    }
+}
+
+export default EVAAP;
