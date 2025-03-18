e55ff6889b2741f4c5bebcd8f81e5b2119fe3af4
PRAVEEN-PUVVALA <praveen.p@hiscopetech.com>
2025-03-18 22:11:20 +0530

    header and footer components created

--- src/registrationPage.jsx
+++ src/registrationPage.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import './registrationPage.css';
 
-import EVEAP_logo from '../public/images/logo/EVAAP_LOGO.png';
-import Twitter_logo from '../public/images/logo/twitter.png';
-import Instagram_logo from '../public/images/logo/instagram-circle.png';
-import LinkedIn_logo from '../public/images/logo/linkedin-circled.png';
+
 import PDF_IMAGE from '../public/images/logo/PDF_IMAGE.png';
+import Header from './assets/Components/header';
+import Fotter from './assets/Components/fotter';
 
 function SignupForm() {
   const [formData, setFormData] = useState({
@@ -109,54 +108,9 @@ function SignupForm() {
   };
 
   return (
+    <>
+    <Header />
     <form className="signup-page" onSubmit={handleSubmit}>
-      <header className="header-section">
-        <div className="flex-container">
-          <div className="logo-wrapper">
-            <img src={EVEAP_logo} alt="Company Logo" className="logo" />
-          </div>
-          <nav className="nav-content">
-            <div className="nav-wrapper">
-              <div className="nav-header">
-                <h1 className="main-title">EMPLOYMENT VERIFICATION AND ACADEMIC PROOFING</h1>
-                <select id="lang" name="languages">
-                  <option value="apple">Languages</option>
-                  <option value="apple">English</option>
-                  <option value="apple">Spanish</option>
-                  <option value="banana">German</option>
-                  <option value="orange">Spain</option>
-                  <option value="grape">Hindi</option>
-                  <option value="mango">Telugu</option>
-                </select>
-              </div>
-              <div className="nav-links">
-                <a href="./index.html" className="nav-link">
-                  Home
-                </a>
-                <a href="./contact us.html" className="nav-link">
-                  Contact Us
-                </a>
-                <a href="./about us page.html" className="nav-link">
-                  About Us
-                </a>
-                <a href="./service page.html" className="nav-link">
-                  Our Services
-                </a>
-              </div>
-              <div className="login">
-                <div className="login2">
-                  <a href="./common signup page.html">Login</a>
-                </div>
-              </div>
-              <div className="utility-links">
-                <button type="button" className="help-btn">
-                  Help
-                </button>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </header>
       <h2 className="signup-title">REGISTRATION FORM</h2>
       <main className="main-content">
         <div className="flex-container">
@@ -398,103 +352,10 @@ function SignupForm() {
           SUBMIT
         </button>
       </div>
-      <div className="fotter">
-        <div className="fotter2">
-          <a href="#">
-            <img
-              className="evaap-logo-2-1-ai-brush-removebg-8-nqsg-4-zr-222"
-              src={EVEAP_logo}
-              alt="EVEAP Logo"
-            />
-            <p id="tooltip">Go To Top</p>
-          </a>
-          <a href="https://maps.app.goo.gl/AZg1Rxsktv8cs1Nq5">
-            <div className="contact-us4">
-              <div className="contact-us5">Contact Us:</div>
-            </div>
-            <div className="no-179-down-street-new-york-xxxxxxxxxxx-usa-600096">
-              prashanthi hills
-              <br />
-              meerpet pincode-500058
-              <br />
-              Hyderabad
-              <br />
-              Telangana,India
-            </div>
-          </a>
-          <ul>
-            <div className="quick-links">
-              <div className="quick-links2">Quick Links:</div>
-            </div>
-            <div className="home3">
-              <a href="./index.html">
-                <div className="home2" id="alertParagraph">
-                  Home
-                </div>
-              </a>
-            </div>
-            <div className="contact-us3">
-              <a href="./contact us.html">
-                <div className="contact-us2">Contact Us</div>
-              </a>
-            </div>
-            <div className="about-us3">
-              <a href="./about us page.html">
-                <div className="about-us2">About Us</div>
-              </a>
-            </div>
-            <div className="our-services3">
-              <a href="./service page.html">
-                <div className="our-services2">Our Services</div>
-              </a>
-            </div>
-          </ul>
-          <ul>
-            <div className="tools">
-              <div className="tools2">Tools:</div>
-            </div>
-            <div className="fa-qs">
-              <a href="https://www.example.com">
-                <div className="fa-qs2">FAQs</div>
-              </a>
-            </div>
-            <div className="carrer">
-              <a href="https://www.example.com">
-                <div className="carrer2">Carrer</div>
-              </a>
-            </div>
-            <div className="announces">
-              <a href="https://www.example.com">
-                <div className="announces2">Announces</div>
-              </a>
-            </div>
-          </ul>
-          <div className="follow-us">
-            <div className="follow-us2">Follow Us:</div>
-          </div>
-          <ul className="follow-us-ul">
-            <div className="instagram-circle">
-              <a href="https://www.instagram.com/evaap_2024" target="_blank">
-                <img className="instagram-circle2" src={Instagram_logo} />
-              </a>
-            </div>
-            <div className="linked-in-circled">
-              <a
-                href="https://www.linkedin.com/in/evaap-3a2965340/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-                target="_blank"
-              >
-                <img className="linked-in-circled2" src={LinkedIn_logo} />
-              </a>
-            </div>
-            <div className="twitter">
-              <a href="https://x.com/evvap415664?t=L4ihkZ-4E8FNY8k8RkY9Bg" target="_blank">
-                <img className="twitter2" src={Twitter_logo} />
-              </a>
-            </div>
-          </ul>
-        </div>
-      </div>
+      
     </form>
+    <Fotter />
+    </>
   );
 }
 
