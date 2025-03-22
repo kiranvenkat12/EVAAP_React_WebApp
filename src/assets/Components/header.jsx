b04465ae4b7729a061b924f6d1ea7c7ebe5cd038
PRAVEEN-PUVVALA <praveen.p@hiscopetech.com>
2025-03-22 23:26:21 +0530

    haeder part

--- src/assets/Components/header.jsx
+++ src/assets/Components/header.jsx
@@ -27,8 +27,8 @@ const Header = () => {
                      } >
                     <ul className='header_div_navlink_ul'>
                         <li>
-                            <NavLink to='/home' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Home</NavLink>
-                            <NavLink to='/registrationPage' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Conatact us</NavLink>
+                            <NavLink to='/' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Home</NavLink>
+                            <NavLink to='/registrationPage' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Contact us</NavLink>
                             <NavLink to='/' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>About us</NavLink>
                             <NavLink to='/' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Our Services</NavLink>
                             <NavLink to='/' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Our Founders</NavLink>
