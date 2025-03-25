9416bce8e38cc5733607b4ca679a61c47550869e
PEAVEEN <praveen.p@hiscopetech.com>
2025-03-25 13:13:31 +0000

    home page devloped

--- src/assets/Components/header.jsx
+++ src/assets/Components/header.jsx
@@ -1,53 +1,86 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import './header.css';
-import EVEAP_logo from '../../../public/images/logo/EVAAP_LOGO.png';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./header.css";
+import EVEAP_logo from "../../../public/images/logo/EVAAP_LOGO.png";
 import { FaBars } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
-
-
 const Header = () => {
-    const [isOpen,setIsOpen]=useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleButton = () =>{
-            setIsOpen(!isOpen);
-     }
-    return (
-        <header className="header-section">
-            <div className="flex-container">
-                <div className="header-logo-wrapper">
-                    <img src={EVEAP_logo} alt="Company Logo" className="header-logo" />
-                </div>
-                <h2 className="header_h2">
-                    EMPLOYMENT VERIFICATION AND ACADEMIC PROOFING
-                </h2>
-                    <nav  className={
-                        isOpen?"header_div_navlink":"header_div_active_navlink"
-                     } >
-                    <ul className='header_div_navlink_ul'>
-                        <li>
-                            <NavLink to='/home' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Home</NavLink>
-                            <NavLink to='/' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>About us</NavLink>
-                            <NavLink to='/contact_usPage' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Contact us</NavLink>
-                            <NavLink to='/' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Our Services</NavLink>
-                            <NavLink to='/registrationPage' className={({isActive})=>isActive?"header_active_navlink":"header_navlink"}>Our Founders</NavLink>
-                        
-                        </li>
-                    </ul>
-                    </nav>
+  const toggleButton = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <header className="header-section">
+      <div className="flex-container">
+        <div className="header-logo-wrapper">
+          <img src={EVEAP_logo} alt="Company Logo" className="header-logo" />
+        </div>
+        <h2 className="header_h2">
+          EMPLOYMENT VERIFICATION AND ACADEMIC PROOFING
+        </h2>
+        <nav
+          className={
+            isOpen ? "header_div_navlink" : "header_div_active_navlink"
+          }
+        >
+          <ul className="header_div_navlink_ul">
+            <li>
+              <NavLink
+                to="/home"
+                className={({ isActive }) =>
+                  isActive ? "header_active_navlink" : "header_navlink"
+                }
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "header_active_navlink" : "header_navlink"
+                }
+              >
+                About us
+              </NavLink>
+              <NavLink
+                to="/contact_usPage"
+                className={({ isActive }) =>
+                  isActive ? "header_active_navlink" : "header_navlink"
+                }
+              >
+                Contact us
+              </NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "header_active_navlink" : "header_navlink"
+                }
+              >
+                Our Services
+              </NavLink>
+              <NavLink
+                to="/registrationPage"
+                className={({ isActive }) =>
+                  isActive ? "header_active_navlink" : "header_navlink"
+                }
+              >
+                Our Founders
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
-                    <button className='header_menu_bar_btn' onClick={toggleButton}>{
-                   isOpen? <ImCross />:<FaBars />
-                        }
-                    </button>
-                    <div className='header_div_login'>
+        <button className="header_menu_bar_btn" onClick={toggleButton}>
+          {isOpen ? <ImCross /> : <FaBars />}
+        </button>
+        {/* <div className='header_div_login'>
                          <button className='header_div_login_btn'>Login</button>
                          <button className='header_div_help_btn'>Help</button>
-                    </div>
-            </div>
-        </header>
-    );
+                    </div> */}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
