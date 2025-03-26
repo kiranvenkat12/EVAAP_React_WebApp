c029c628978e3ad79eaf8bb07025a67984a6e339
PEAVEEN <praveen.p@hiscopetech.com>
2025-03-26 05:50:06 +0000

    founder page develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Header from './assets/Components/header';
-import Fotter from './assets/Components/fotter';
-import Home from './home';
-import SignupForm from './registrationPage';
-import ContactUs from './contact_usPage';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Header from "./assets/Components/header";
+import Fotter from "./assets/Components/fotter";
+import Home from "./home";
+import SignupForm from "./registrationPage";
+import ContactUs from "./contact_usPage";
+import FounderPage from "./founderPage";
 
 function App() {
   return (
@@ -14,7 +20,8 @@ function App() {
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/registrationPage" element={<SignupForm />} />
-        <Route path="/contact_usPage" element={<ContactUs/>}/>
+        <Route path="/contact_usPage" element={<ContactUs />} />
+        <Route path="/founderPage" element={<FounderPage />} />
       </Routes>
       <Fotter />
     </Router>
