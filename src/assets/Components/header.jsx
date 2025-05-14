529cc1a91e29a2d52e80ca5680724a478f20703f
kiranvenkat12 <beesettyk@gmail.com>
2025-05-14 20:34:19 +0530

    Update header.jsx

--- src/assets/Components/header.jsx
+++ src/assets/Components/header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
                   isActive ? "header_active_navlink" : "header_navlink"
                 }
               >
-                Homee
+                Home
               </NavLink>
               <NavLink
                 to="/aboutUsPage"
